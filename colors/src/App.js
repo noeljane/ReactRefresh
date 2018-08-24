a2e7cb4fdf404a5c95f905bf46c0734611aa75e6
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-23 22:37:26 -0700

    add boxes and sliders

--- colors/src/App.js
+++ colors/src/App.js
@@ -5,7 +5,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>box</div>
+        <div class="container">
+        <div class="slidecontainer">
+          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+        </div>
+          <div class="box" id="red">box</div>
+        <div class="slidecontainer">
+          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+        </div>
+          <div class="box"id="green">box</div>
+          <div class="slidecontainer">
+          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+        </div>
+          <div class="box" id="blue">box</div>
+        </div>
+        
       </div>
     );
   }
