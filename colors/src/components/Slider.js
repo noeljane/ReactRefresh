376dfd7c31ec0bac12b34a2ba221ad9e73cc29e6
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-27 23:04:02 -0700

    bind change value function

--- colors/src/components/Slider.js
+++ colors/src/components/Slider.js
@@ -7,10 +7,16 @@ class Slider extends Component {
         green: 0,
         blue: 0,
         divStyle: {
-            color:this.red,
+            backgroundColor:[this.red,this.green, this.blue]
         }
         
     }
+
+    changeValue (evt) {
+        evt.preventDefault();
+        console.log(this)
+        console.log("I'm running changeValue function.")
+    };
     
     render(){
         console.log("printing state")
@@ -19,15 +25,15 @@ class Slider extends Component {
         <div className="container">
             <div className="box square">This is the target square</div>
             <div className="slidecontainer">
-            <input type="range" min="1" max="100"  className="slider" id="myRange"/>
+            <input type="range" min="1" max="100"  className="slider" id="myRange" onChange={this.changeValue.bind(this)}/>
             </div>
             <div className="box" id="red">box</div>
             <div className="slidecontainer">
-            <input type="range" min="1" max="100"  className="slider" id="myRange"/>
+            <input type="range" min="1" max="100"  className="slider" id="myRange" onChange={this.changeValue.bind(this)}/>
             </div>
             <div className="box"id="green">box</div>
             <div className="slidecontainer">
-            <input type="range" min="1" max="100"  className="slider" id="myRange"/>
+            <input type="range" min="1" max="100"  className="slider" id="myRange" onChange={this.changeValue.bind(this)}/>
             </div>
             <div className="box" id="blue">box</div>
         </div> 
