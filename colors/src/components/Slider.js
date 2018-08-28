2ae46186bf68e8704810d7e1d2bd5d8493ed726f
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-27 22:14:32 -0700

    add state to Slider component

--- colors/src/components/Slider.js
+++ colors/src/components/Slider.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import './Slider.css';
 
 class Slider extends Component {
+    state = {
+        
+    }
     
     render(){
         return (
-            <div class="container">
-        <div class="slidecontainer">
-          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
-        </div>
-          <div class="box" id="red">box</div>
-        <div class="slidecontainer">
-          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
-        </div>
-          <div class="box"id="green">box</div>
-          <div class="slidecontainer">
-          <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
-        </div>
-          <div class="box" id="blue">box</div>
+        <div class="container">
+            <div class="slidecontainer">
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+            </div>
+            <div class="box" id="red">box</div>
+            <div class="slidecontainer">
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+            </div>
+            <div class="box"id="green">box</div>
+            <div class="slidecontainer">
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+            </div>
+            <div class="box" id="blue">box</div>
         </div> 
         )
     }
