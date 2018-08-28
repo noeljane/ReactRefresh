bbb8ed6f61cb96f251360c00a549a5f7378fd616
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-08-27 22:19:06 -0700

    add target container

--- colors/src/components/Slider.js
+++ colors/src/components/Slider.js
@@ -9,16 +9,17 @@ class Slider extends Component {
     render(){
         return (
         <div class="container">
+            <div class="box square">This is the target square</div>
             <div class="slidecontainer">
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+            <input type="range" min="1" max="100"  class="slider" id="myRange"/>
             </div>
             <div class="box" id="red">box</div>
             <div class="slidecontainer">
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+            <input type="range" min="1" max="100"  class="slider" id="myRange"/>
             </div>
             <div class="box"id="green">box</div>
             <div class="slidecontainer">
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+            <input type="range" min="1" max="100"  class="slider" id="myRange"/>
             </div>
             <div class="box" id="blue">box</div>
         </div> 
